23d4f1e2cde6d3c65a52128af882b27e257c94ad
Tonysu <tonysu@lagou.com>
2021-07-19 17:07:06 +0800

    update

---
+++ src/main/java/leetcode/arrary/LeetCode_39_CombinationSum.java
@@ -0,0 +1,42 @@
+package leetcode.arrary;
+
+import java.util.List;
+
+/**
+ * 题目：组合总和
+ *
+ * 描述：给定一个无重复元素的正整数数组 candidates 和一个正整数 target ，找出 candidates 中所有可以使数字和为目标数 target 的唯一组合。
+ *      candidates 中的数字可以无限制重复被选取。如果至少一个所选数字数量不同，则两种组合是唯一的。
+ *      对于给定的输入，保证和为 target 的唯一组合数少于 150 个。
+ *
+ *
+ * 示例：
+ * 输入：candidates = [2,3,6,7], target = 7
+ * 输出：[[7],[2,2,3]]
+ *
+ * 示例：
+ * 输入：candidates = [2,3,5], target = 8
+ * 输出：[[2,2,2,2],[2,3,3],[3,5]]
+ *
+ *
+ * 提示：
+ *      1 <= candidates.length <= 30
+ *      1 <= candidates[i] <= 200
+ *      candidate 中的每个元素都是独一无二的。
+ *      1 <= target <= 500
+ *
+ *
+ * @author tonysu
+ * @version 1.0
+ * @since 2021/7/19 5:02 下午
+ */
+public class LeetCode_39_CombinationSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return null;
+    }
+}
